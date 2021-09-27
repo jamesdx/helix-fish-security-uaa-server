6a8a8456d7960e764666c681051532dd392673ac
zhe.d.wang <zhe.d.wang@accenture.com>
2021-09-27 16:41:47 +0800

    Add Spring Boot Basic Dependency

---
+++ src/main/java/org/helix/fish/security/uaa/HelixFishSecurityUaaApplication.java
@@ -0,0 +1,11 @@
+package org.helix.fish.security.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelixFishSecurityUaaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelixFishSecurityUaaApplication.class,args);
+    }
+}
