803a8317b9256bc3c1a67bc2d0e33bfe3db62333
zhe.d.wang <zhe.d.wang@accenture.com>
2021-09-28 16:22:30 +0800

    change the package name and moves the Spring Application Main Class into it.

--- src/main/java/org/helix/fish/security/uaa/authserver/HelixFishSecurityUaaAuthServerApplication.java
+++ src/main/java/org/helix/fish/security/uaa/authserver/HelixFishSecurityUaaAuthServerApplication.java
@@ -1,4 +1,4 @@
-package org.helix.fish.security.uaa;
+package org.helix.fish.security.uaa.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
