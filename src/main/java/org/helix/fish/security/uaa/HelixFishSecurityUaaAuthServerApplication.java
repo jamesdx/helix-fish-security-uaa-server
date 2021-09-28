1f7cf862610747d5e78d245c9c67831943d12bb5
zhe.d.wang <zhe.d.wang@accenture.com>
2021-09-28 16:20:48 +0800

    Rename the project to helix-fish-security-uaa-auth-server

--- src/main/java/org/helix/fish/security/uaa/HelixFishSecurityUaaAuthServerApplication.java
+++ src/main/java/org/helix/fish/security/uaa/HelixFishSecurityUaaAuthServerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelixFishSecurityUaaApplication {
+public class HelixFishSecurityUaaAuthServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HelixFishSecurityUaaApplication.class,args);
+        SpringApplication.run(HelixFishSecurityUaaAuthServerApplication.class,args);
     }
 }
